eae833d8a941dca5f1fcaa93d989b397f831ec31
Aloego <ocheialoysius@gmail.com>
2025-06-10 09:12:17 +0100

    Manual sync at Tue Jun 10 09:12:17 WAT 2025

--- js/car-details.js
+++ js/car-details.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("carPrice").textContent = `Price: ${car.price}` ;
       document.getElementById("carMileage").textContent = `Mileage: ${car.mileage}` || " ";
       document.getElementById("carLocation").textContent = `Location: ${car.location}`;
-      document.getElementById("carDescription").textContent = `${car.description}`;
+      document.getElementById("carDescription").innerHTML = `${car.description}`;
       document.getElementById("carCondition").textContent = `Condition: ${car.condition}`  || "Not specified";
 
       
